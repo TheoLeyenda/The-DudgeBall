c2d297080f80a8bc34f28d7fddb8a5ddddb47f93
Unknown <theoleyenda1997@gmail.com>
2018-10-15 22:15:47 -0300

    Assets-meta

---
+++ TP Dodgeball/Assets/MyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MyScript : MonoBehaviour {
+
+    // Use this for initialization
+    public FixedJoystick MoveJoystick;
+    public FixedButton JumpButton;
+    public FixedTouchField TouchField;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var fps = GetComponent<RigidbodyFirstPersonController>();
+        fps.RunAxis = MoveJoystick.inputVector;
+        fps.JumpAxis = JumpButton.Pressed;
+        fps.mouseLook.LookAxis = TouchField.TouchDist;
+	}
+}
