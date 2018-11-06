69aa37054316a59186adba66b012e70c0b9cbd97
TheoLeyenda <theoleyenda1997@gmail.com>
2018-11-06 12:27:30 -0300

    Shaders cambiados y Kraken en progreso...

---
+++ TP Dodgeball/Assets/Scripts/Enemigos/Kraken.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kraken : Enemigo {
+
+    // Use this for initialization
+    private States estados;
+    private Rigidbody rig;
+    private Vector3 posJugador;
+    private int id = 0;
+
+    public float FuerzaImpulsoMov;
+    public Transform[] waypoints;
+    public enum States
+    {
+        Nadando = 0,
+        Atacar,
+        Retirse,
+        Count
+    }
+
+    public enum Events
+    {
+        EnVista = 0,
+        FueraDeVista,
+        EnRangoDeAtaque,
+        FueraDelRangoDeAtaque,
+        LuegoDeAtacar,
+        VolviendoQuieto,
+        Count
+    }
+
+    void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void UpdateStates()
+    {
+        switch ((int)estados)
+        {
+            case (int)States.Nadando:
+                Nadar();
+                break;
+            case (int)States.Atacar:
+                Atacar();
+                break;
+            case (int)States.Retirse:
+                Retirarse();
+                break;
+        }
+    }
+    public void Nadar()
+    {
+        if (waypoints.Length > 0)
+        {
+            if (waypoints[id] != null)
+            {
+                Vector3 target = waypoints[id].position;
+                transform.LookAt(target);
+                if (id >= waypoints.Length)
+                {
+                    id = 0;
+                }
+
+            }
+        }
+        else
+        {
+            id = 0;
+        }
+    }
+    public void Atacar()
+    {
+
+    }
+    public void Retirarse()
+    {
+
+    }
+    public void UpdatePositionPlayer()
+    {
+        if (Jugador.GetJugador() != null)
+        {
+            posJugador = Jugador.GetJugador().transform.position;
+        }
+    }
+}
