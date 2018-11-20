5fb1fc8997d90f11103e1de1ab48d79f14a4257a
TheoLeyenda <theoleyenda1997@gmail.com>
2018-11-20 20:07:19 -0300

    Primera parte Nivel 3 En progreso...

---
+++ TP Dodgeball/Assets/Scripts/OTROS/PuertaRejas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaRejas : MonoBehaviour {
+
+    // Use this for initialization
+    private Animation animacion;
+    public AnimationClip animationClip;
+    void Start ()
+    {
+        animacion = GetComponent<Animation>();
+        animacion.clip = animationClip;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void AbrirPuerta()
+    {
+        animacion.clip = animationClip;        
+        animacion.Play();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "PararAnimacion")
+        {
+            Debug.Log("TREMENDA COLICION");
+            animacion.Stop();
+        }
+    }
+}
---
+++ TP Dodgeball/Assets/Scripts/Plataformas/PlacaDePresion.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacaDePresion : MonoBehaviour {
+
+	// Use this for initialization
+    private Animation animacion;
+    public AnimationClip AnimationClip;
+    public GameObject[] objectosApagar;
+    public Enemigo Enemigo;
+    public PuertaRejas puerta;
+    public bool checkAbrirPuerta;
+    private bool unaVez;
+	void Start () {
+        animacion = GetComponent<Animation>();
+        if (Enemigo != null)
+        {
+            Enemigo.gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(checkAbrirPuerta)
+        {
+            if (Enemigo != null)
+            {
+                if (Enemigo.vida <= 0)
+                {
+                    if(puerta != null && !unaVez)
+                    {
+                        puerta.AbrirPuerta();
+                        unaVez = true;
+                    }
+                }
+            }
+        }
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        animacion.clip = AnimationClip;
+        animacion.Play();
+        for (int i = 0; i < objectosApagar.Length; i++)
+        {
+            objectosApagar[i].SetActive(false);
+        }
+        if (Enemigo != null)
+        {
+            Enemigo.gameObject.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        animacion.transform.localScale = new Vector3(transform.localScale.x,0.1f,transform.localScale.z);
+    }
+
+}
