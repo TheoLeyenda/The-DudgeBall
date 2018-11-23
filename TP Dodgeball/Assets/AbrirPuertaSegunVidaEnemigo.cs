461d96561bc8be1406921252b4bfad5a27668e43
TheoLeyenda <theoleyenda1997@gmail.com>
2018-11-23 19:27:40 -0300

    Nivel 3 - parte 3 en progreso...

--- TP Dodgeball/Assets/AbrirPuertaSegunVidaEnemigo.cs
+++ TP Dodgeball/Assets/AbrirPuertaSegunVidaEnemigo.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class AbrirPuertaSegunVidaEnemigo : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    public PuertaRejas puertaRejas;
+    public Enemigo[] enemigos;
+    private int cantEnemigosMuertos;
+    private bool abrirPuerta;
 	void Start () {
-		
+        abrirPuerta = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CheckEnemigosMuertos();
+        CheckAbrirPuerta();
 	}
+    public void CheckAbrirPuerta()
+    {
+        if(abrirPuerta)
+        {
+            if(puertaRejas != null)
+            {
+                puertaRejas.SetAbrirPuerta(true);
+            }
+        }
+    }
+    public void CheckEnemigosMuertos()
+    {
+        for(int i = 0; i< enemigos.Length; i++)
+        {
+            if (enemigos[i] != null)
+            {
+                if (enemigos[i].vida <= 0)
+                {
+                    cantEnemigosMuertos++;
+                }
+            }
+        }
+        if(cantEnemigosMuertos >= enemigos.Length)
+        {
+            abrirPuerta = true;
+        }
+        else
+        {
+            cantEnemigosMuertos = 0;
+        }
+    }
 }
