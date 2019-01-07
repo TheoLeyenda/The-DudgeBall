66a01b59dd64ab8da2153ee10faab67614443f77
TheoLeyenda <theoleyenda1997@gmail.com>
2019-01-06 22:28:45 -0300

    Pelota Achicada e IMPLEMENTADA

--- TP Dodgeball/Assets/Scripts/Spawner/SpawnerPlayer.cs
+++ TP Dodgeball/Assets/Scripts/Spawner/SpawnerPlayer.cs
@@ -16,18 +16,13 @@ public class SpawnerPlayer : MonoBehaviour {
 #elif UNITY_STANDALOVE
         if (Jugador_Windows != null)
         {
-            Instantiate(Jugador_Windows, this.transform.position, Quaternion.identity);
+            Instantiate(player_Windows, this.transform.position, Quaternion.identity);
         }
 #elif UNITY_ANDROID
         if(Jugador_Android != null)
         {
-            Instantiate(Jugador_Android, this.transform.position, Quaternion.identity);
+            Instantiate(player_Android, this.transform.position, Quaternion.identity);
         }
 #endif
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
