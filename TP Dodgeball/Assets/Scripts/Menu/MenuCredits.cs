5354dd85e6477df5d5f3c330a680c9690a2d3adc
TheoLeyenda <theoleyenda1997@gmail.com>
2019-01-15 03:45:42 -0300

    Version para PC LISTA

--- TP Dodgeball/Assets/Scripts/Menu/MenuCredits.cs
+++ TP Dodgeball/Assets/Scripts/Menu/MenuCredits.cs
@@ -10,35 +10,28 @@ public class MenuCredits : MonoBehaviour {
     public GameObject buttonPinUp;
     public float speed;
     private float y;
-    private float yButtonPinUp;
     public float limit;
     //public float riseTime;
     void Start() {
         y = credits.transform.position.y;
-        yButtonPinUp = buttonPinUp.transform.position.y;
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(buttonPinUp.transform.position.y);
-        if (buttonPinUp.transform.position.y < limit)
+        if (credits.activeSelf == true)
         {
-            if (credits.activeSelf == true)
-            {
-                MoveVertical();
-            }
+            MoveVertical();
         }
     }
     public void CreditsMenu()
     {
         credits.SetActive(true);
+        buttonPinUp.SetActive(true);
         completedGame.SetActive(false);
     }
     public void MoveVertical()
     {
         y = y + Time.deltaTime * speed;
-        yButtonPinUp = yButtonPinUp + Time.deltaTime * speed;
         credits.transform.position = new Vector3(credits.transform.position.x, y, credits.transform.position.z);
-        buttonPinUp.transform.position = new Vector3(buttonPinUp.transform.position.x, yButtonPinUp, buttonPinUp.transform.position.z);
     }
 }
